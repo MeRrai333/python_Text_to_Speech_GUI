eb9ee1693a1a73055557964acd023014d798ed4d
MeRrai <113435542+MeRrai333@users.noreply.github.com>
2023-02-12 21:03:33 +0700

    update gui

--- TTS.py
+++ TTS.py
@@ -6,7 +6,7 @@ import gtts.lang
 lang_arr = gtts.lang.tts_langs()
 
 def TextToSpeech(text, lg):
-    file_name = 'speak.mp3'
+    file_name = "speak.mp3"
     lang = list(lang_arr.keys()) [list(lang_arr.values()).index(lg)]
     tts = gTTS(text, lang=lang)
     tts.save(file_name)
--- gui.py
+++ gui.py
@@ -13,6 +13,11 @@ font_size1 = 16
 font_size2 = 12
 font_size3 = 11
 
+main_bg = '#4A4A4A'
+enter_text_bg = '#878787'
+font_color = '#FFFFFF'
+onhold_bg = '#c3c3c3'
+
 """ Send data to gTTS """
 def ToTTS():
     TTS.TextToSpeech(enter_text.get(1.0, "end-1c"), lang_sele.get())
@@ -30,28 +35,32 @@ lang_sele.set("English")
 """ Variable for select language """
 
 """ Edit data for GUI """
-head_label = tk.Label(text="Enter text for text to speech", font=(font_fam, font_size1))
+head_label = tk.Label(text="Enter text for text to speech", font=(font_fam, font_size1), bg=main_bg, fg=font_color)
 
-enter_text = tk.Text(window, font=(font_fam, font_size3), height=5, width=40, )
+enter_text = tk.Text(window, font=(font_fam, font_size3), height=7, width=40, padx=5, pady=5, bg=enter_text_bg, highlightthickness=0)
 
-button_speech = tk.Button(text="Speech!", font=(font_fam, font_size2), width=10, height=2, command=ToTTS)
+button_speech = tk.Button(text="Speech!", font=(font_fam, font_size2), width=10, height=2, command=ToTTS, bg=enter_text_bg, fg=font_color, activebackground=onhold_bg)
 
-lang_label = tk.Label(text="Select language", font=(font_fam, font_size3))
+lang_label = tk.Label(text="Select language", font=(font_fam, font_size3), bg=main_bg, fg=font_color)
 
-lang_drop = tk.OptionMenu( window , lang_sele , *lang_list )
+lang_drop = tk.OptionMenu( window , lang_sele , *lang_list)
+lang_drop.config(bg=enter_text_bg)
+lang_drop.config(fg=font_color)
+lang_drop.config(highlightthickness=0)
+lang_drop.config(activebackground=onhold_bg)
 """ Edit data for GUI """
 
 """ Place GUI """
 lang_label.grid(column=0, row=0, sticky=tk.W, padx=20, pady=3)
 lang_drop.grid(column=0, row=1, sticky=tk.W, padx=(20,10), pady=3)
 
-head_label.grid(column=0, row=2, sticky=tk.W, padx=20, pady=(20,5))
+head_label.grid(column=0, row=2, sticky=tk.W, padx=20, pady=(10,5))
 
 enter_text.grid(column=0, row=3, sticky=tk.W, padx=(20,10), pady=0)
 button_speech.grid(column=1, row=3, sticky=tk.W, padx=0, pady=0)
 """ Place GUI """
 
-
+window.configure(bg=main_bg)
 window.minsize(window_width, window_height)
 window.maxsize(window_width, window_height)
 window.mainloop()
